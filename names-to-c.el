c56e9312a6c68e12ab183f5d73c3dc228b25a7cd
davem8 <pizzadave108@gmail.com>
2017-02-23 23:40:44 +0000

    mostly works still needs testing in the wild

--- names-to-c.el
+++ names-to-c.el
@@ -2,45 +2,65 @@
 ;; to a c(var1,var2, ... ,varn)
 ;; David Morrisroe
 
+;; the R function names will out put the names of all the fields
+;; in the format below
+;; > names(taxi)
+;;  [1] "pickup_ad_dist" "pickup_ed_dist" "pickup_hour"    "pickup_day"    
+;;  [5] "pickup_month"   "avg_total"      "sd_total"       "total_amt"     
+;;  [9] "avg_tip"        "sd_tip"         "total_tip"      "num_trips"     
+
+;; this elisp function will change this to a comma seperated
+;; list all on one line
+;; like so
+
+;; pickup_ad_dist, pickup_ed_dist, pickup_hour, pickup_day, pickup_month, avg_total, sd_total, total_amt, avg_tip, sd_tip, total_tip, num_trips,
+
+;; TODO
+;; there's still a traling comma
+;; I Think this could all be don't with just one regexp
+
 (defun names-to-c (start-point end-point)
   (interactive "r")
   (save-excursion
     (save-restriction
-    (message (concat "beg " (number-to-string start-point)
-		     " end " (number-to-string end-point)))
-    (defvar beg start-point)
-    (defvar end end-point)
-    (narrow-to-region beg end)
-    (goto-char (point-min)
-    (while (and (< (point) end)
-		(re-search-forward
-		 "[[:space:]]?\\[[0-9]+\\][[:space:]]+" nil t))
-      (replace-match ""))
-    (goto-char beg)
-    (while (and (< (point) end)
-		(re-search-forward
-		 "[[:space:]]+" nil t)
-		(replace-match "\n")))
-        (message (concat "beg " (number-to-string beg)
-		     " end " (number-to-string end)))))
-	;; (goto-char (beg))
-	;; (while (and (< (point) end)
-	;; 	    (re-search-	
-	;; 	     "\"\\(.*\\)\"" nil t)
-	;; 	    (replace-match "\\1")))
-	
-	;; (goto-char (beg))
-	;; (while (and (< (point) end)
-	;; 	    (re-search-forward
-	;; 	     "\n" nil t)
-	;; 	    (replace-match ", ")))
-	;; (goto-char (beg))
-	;; (while (and (< (point) end)
-	;; 	    "^," nil t)
-	;;   (replace-match ""))
-	;; (goto-char (beg))
-	;; (while (and (< (point) end)
-	;; 	"\\(.+\\)" nil t)
-	;;   (replace-match "c(\\1)"))
-	))
-
+      (narrow-to-region start-point end-point)
+      ;;(message (number-to-string end-point))
+      (goto-char (point-min))
+      ;; match: the [8] at the start of each line
+      ;; replace: blank
+      (while (re-search-forward
+	      "[[:space:]]?\\[[0-9]+\\][[:space:]]+" nil t)
+	(replace-match ""))
+      (goto-char (point-min))
+      ;; match: the spaces between the fields on the same line
+      ;; replace: new line
+      (while (re-search-forward
+	      "[[:space:]]+" nil t)
+	(replace-match "\n"))
+      (goto-char (point-min))
+      ;; match: field name
+      ;; replacd: remove the double quotes "field-name"
+      (while (re-search-forward
+	      "\"\\(.*\\)\"" nil t)
+	(replace-match "\\1"))
+      (goto-char (point-min))
+      ;; remove the blank lines
+      (while (re-search-forward
+	      "^\n" nil t)
+	(replace-match ""))
+      (goto-char (point-min))
+      ;; replace the new lines with ,
+      ;; this makes all the fields on one line
+      (while (re-search-forward
+	      "\n" nil t)
+	(replace-match ", "))
+      (goto-char (point-min))
+      ;; remove any starting ,
+      (while (re-search-forward
+	      "^," nil t)
+	(replace-match ""))
+      (goto-char (point-max))
+      ;;remove traling , and any traling white-space
+      (while (re-search-forward
+	      ",\s?$" nil t)
+	(replace-match "")))))
