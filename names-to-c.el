97f64fd63bcd84f92268a51c7a7b29546156658f
davem8 <pizzadave108@gmail.com>
2017-02-23 20:58:09 +0000

    Make work on region

--- names-to-c.el
+++ names-to-c.el
@@ -2,24 +2,27 @@
 ;; to a c(var1,var2, ... ,varn)
 ;; David Morrisroe
 
-(defun names-to-c ()
-  (interactive)
+(defun names-to-c (start-point end-point)
+  (interactive "r")
   (save-excursion
-    (let*
-	(beg (region-beginning))
-      (end (region-end))
-      (goto-char (beg))
-      (while (and (< (point) (end))
-		  (re-search-forward
-		   "[[:space:]]?\\[[0-9]+\\][[:space:]]+" nil t))
-	(replace-match "")))))
-
-	;; (goto-char (beg))
-	;; (while (and (< (point) end)
-	;; 	    (re-search-forward
-	;; 	     "[[:space:]]+" nil t)
-	;; 	    (replace-match "\n")))
-
+    (save-restriction
+    (message (concat "beg " (number-to-string start-point)
+		     " end " (number-to-string end-point)))
+    (defvar beg start-point)
+    (defvar end end-point)
+    (narrow-to-region beg end)
+    (goto-char (point-min)
+    (while (and (< (point) end)
+		(re-search-forward
+		 "[[:space:]]?\\[[0-9]+\\][[:space:]]+" nil t))
+      (replace-match ""))
+    (goto-char beg)
+    (while (and (< (point) end)
+		(re-search-forward
+		 "[[:space:]]+" nil t)
+		(replace-match "\n")))
+        (message (concat "beg " (number-to-string beg)
+		     " end " (number-to-string end)))))
 	;; (goto-char (beg))
 	;; (while (and (< (point) end)
 	;; 	    (re-search-	
